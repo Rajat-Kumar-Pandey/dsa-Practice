8cd27f47263a44fa1fdf53f11f8415521de1a543
Rajat Kumar Pandey <rajatkumarpandey40gmail.com>
2024-10-31 16:33:03 +0530

    Initial commit

---
+++ Array/Reverse_Words_in_a_String.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Split the string into words, filter out empty strings
+        words = s.split()
+        # Reverse the list of words
+        words.reverse()
+        # Join the words with a single space and return
+        return ' '.join(words)
+
+
+solution = Solution()
+input_str1 = "the sky is blue"
+output1 = solution.reverseWords(input_str1)
+print("Output:", output1)  # Output: "blue is sky the"
+
+# Test case 2
+input_str2 = "  hello world  "
+output2 = solution.reverseWords(input_str2)
+print("Output:", output2)  # Output: "world hello"
